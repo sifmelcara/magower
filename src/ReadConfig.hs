14ecc9ffbcc20f5d07c9a3a7083c7c13b88bdfce
sifmelcara <rbdn@9mm.davy.tw>
2015-02-25 11:04:42 +0800

    finish readURI

--- src/ReadConfig.hs
+++ src/ReadConfig.hs
@@ -1,5 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase#-}
 
 module ReadConfig where
 
+import Network.URI
+import Data.Maybe
+import qualified Data.Configurator as C
 
+readURI :: IO URI
+readURI = do
+    config <- C.load [C.Required "$(HOME)/.magower"]
+    C.lookup config "servpath" >>= \case
+        Just servpath -> return . fromJust $ parseURI servpath
+        Nothing -> error "please edit config file(.magower)."
 
