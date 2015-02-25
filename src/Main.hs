ec72be27611574f8f73c4842f5c611e2ac364aa3
sifmelcara <rbdn@9mm.davy.tw>
2015-02-25 21:26:07 +0800

    finish ending

--- src/Main.hs
+++ src/Main.hs
@@ -4,24 +4,29 @@
 import GetLink
 import Communicate
 import ReadConfig
+import ShowProgress
 
 import Control.Monad
 import Control.Applicative
 import Data.Maybe
 import System.Environment
 import Control.Concurrent
+import Control.Monad.Loops
 
 main :: IO ()
 main = do
     checkConfig
     link <- getLink
     tid <- untilJust $ throwLink link
-    forever $ putStrLn . show =<< (untilJust $ getProgress tid)
+    whileM_ (checkContinue tid) $ putPrg tid
     return ()
-    
+    where putPrg t = do
+            pgs <- untilJust $ getProgress t
+            showProgress pgs
+            threadDelay 500000
+          checkContinue t = do
+            pgs <- untilJust $ getProgress t
+            return $ pgs < (1-1e-6)
 
-untilJust :: Monad m => m (Maybe a) -> m a
-untilJust m = m >>= \case
-    Nothing -> untilJust m
-    Just x -> return x
-        
+
+
