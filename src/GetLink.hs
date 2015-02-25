38a8f1ed8a515c93e06bc97b9e92af3c4ef2b046
sifmelcara <rbdn@9mm.davy.tw>
2015-02-25 16:09:29 +0800

    get link

--- src/GetLink.hs
+++ src/GetLink.hs
@@ -1,3 +1,8 @@
 
 module GetLink where
 
+getLink :: IO String
+getLink = do
+    putStrLn "Please feed me a magnet link:"
+    getLine
+
--- src/Main.hs
+++ src/Main.hs
@@ -10,12 +10,5 @@ import Control.Concurrent
 
 main :: IO ()
 main = do
-    link <- fmap head getArgs
-    tid <- sentLnk link ""
-    forever $ do
-        sid <- getSesId
-        st <- getStat tid sid
-        putStrLn $ dlProg st
-        threadDelay (1000000)
-    return ()
-
+    checkConfig
+    link <- GetLink
