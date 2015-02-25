399a2b046d8d84adb8a77df44966ad965d8076dd
sifmelcara <rbdn@9mm.davy.tw>
2015-02-25 21:13:00 +0800

    finish progress bar

--- src/ShowProgress.hs
+++ src/ShowProgress.hs
@@ -1,3 +1,17 @@
 
 module ShowProgress where
 
+import System.IO
+import Text.Printf
+
+showProgress :: Double -> IO ()
+showProgress d = putProgress $ genBar d ++ " " ++ (printf "%3d%%" $ ((truncate (d*100))::Int))
+
+putProgress :: String -> IO ()
+putProgress s = hPutStr stderr $ "\r\ESC[K" ++ s
+
+genBar :: Double -> String
+genBar d = "[" ++ brs ++ ">" ++ ws ++ "]"
+    where brs = replicate eqnum '='
+          eqnum = round $ d*50
+          ws = replicate (50-eqnum) ' '
